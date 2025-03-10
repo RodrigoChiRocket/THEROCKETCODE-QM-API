39e02e5847b9b4407069a5d4481eb79057171341
RafaPacheco2003 <115204774+RafaPacheco2003@users.noreply.github.com>
2025-03-10 16:36:30 -0600

    dwsdsd

---
+++ multicotizador-api/src/main/java/com/qualitas/portal/accountfraudesapi/controller/ResultadoCotizacionControlador.java
@@ -0,0 +1,118 @@
+package com.qualitas.portal.accountfraudesapi.controller;
+
+
+import com.qualitas.portal.fraudes.account.application.service.ResultadoCotizacionService;
+import com.qualitas.portal.fraudes.account.domain.dto.ResultadoCotizacionDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/resultados-cotizacion")
+public class ResultadoCotizacionControlador {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResultadoCotizacionControlador.class);
+
+    @Autowired
+    private ResultadoCotizacionService resultadoCotizacionService;
+
+    @PostMapping
+    public ResponseEntity<ResultadoCotizacionDTO> crearResultadoCotizacion(@RequestBody ResultadoCotizacionDTO dto) {
+        // Loguear los datos del request
+        logger.info("Recibiendo solicitud para crear un resultado de cotización. Datos recibidos:" + dto);
+
+
+        // Procesar la solicitud
+        ResultadoCotizacionDTO resultadoDTO = resultadoCotizacionService.crearResultadoCotizacion(dto);
+
+        // Loguear el resultado creado
+        logger.info("Resultado de cotización creado: {}", resultadoDTO);
+        return new ResponseEntity<>(resultadoDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResultadoCotizacionDTO> obtenerResultadoCotizacion(@PathVariable BigDecimal id) {
+        logger.info("Recibiendo solicitud para obtener resultado de cotización con ID: {}", id);
+        ResultadoCotizacionDTO resultadoDTO = resultadoCotizacionService.obtenerResultadoCotizacion(id);
+        if (resultadoDTO != null) {
+            logger.info("Resultado de cotización encontrado: {}", resultadoDTO);
+        } else {
+            logger.warn("No se encontró resultado de cotización con ID: {}", id);
+        }
+        return ResponseEntity.ok(resultadoDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResultadoCotizacionDTO>> listarResultadosCotizacion() {
+        logger.info("Recibiendo solicitud para listar todos los resultados de cotización");
+        List<ResultadoCotizacionDTO> resultados = resultadoCotizacionService.listarResultadosCotizacion();
+        logger.info("Número de resultados de cotización encontrados: {}", resultados.size());
+        return ResponseEntity.ok(resultados);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResultadoCotizacionDTO> actualizarResultadoCotizacion(
+            @PathVariable BigDecimal id, @RequestBody ResultadoCotizacionDTO dto) {
+        logger.info("Recibiendo solicitud para actualizar resultado de cotización con ID: {}. Datos recibidos: {}", id, dto);
+        ResultadoCotizacionDTO resultadoDTO = resultadoCotizacionService.actualizarResultadoCotizacion(id, dto);
+        logger.info("Resultado de cotización actualizado: {}", resultadoDTO);
+        return ResponseEntity.ok(resultadoDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarResultadoCotizacion(@PathVariable BigDecimal id) {
+        logger.info("Recibiendo solicitud para eliminar resultado de cotización con ID: {}", id);
+        resultadoCotizacionService.eliminarResultadoCotizacion(id);
+        logger.info("Resultado de cotización con ID: {} eliminado", id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/verificar-datos")
+    public ResponseEntity<Boolean> verificarExistenciaDeDatos() {
+        logger.info("Recibiendo solicitud para verificar existencia de datos");
+        boolean existenDatos = resultadoCotizacionService.verificarExistenciaDeDatos();
+        logger.info("Existencia de datos: {}", existenDatos);
+        return ResponseEntity.ok(existenDatos);
+    }
+    @GetMapping("/por-cotizacion-clave/{cotizacionClave}")
+    public ResponseEntity<List<ResultadoCotizacionDTO>> obtenerResultadoPorCotizacionClave(@PathVariable BigDecimal cotizacionClave) {
+        logger.info("Recibiendo solicitud para obtener resultado de cotización por clave: {}", cotizacionClave);
+        List<ResultadoCotizacionDTO> resultados = resultadoCotizacionService.obtenerResultadoPorCotizacionClave(cotizacionClave);
+        if (!resultados.isEmpty()) {
+            logger.info("Resultados de cotización encontrados: {}", resultados);
+            return ResponseEntity.ok(resultados);
+        } else {
+            logger.warn("No se encontraron resultados de cotización con clave: {}", cotizacionClave);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscar/{cotizacionClave}/{nombreSeguro}")
+    public ResponseEntity<List<ResultadoCotizacionDTO>> buscarPorNombreSeguroYCotizacionClave(
+            @PathVariable BigDecimal cotizacionClave,
+            @PathVariable String nombreSeguro
+    ) {
+        logger.info("Recibiendo solicitud para buscar resultados por clave de cotización: {} y nombre de seguro: {}", cotizacionClave, nombreSeguro);
+
+        // Llamar al servicio
+        List<ResultadoCotizacionDTO> resultados = resultadoCotizacionService.buscarPorNombreSeguroYCotizacionClave(nombreSeguro, cotizacionClave);
+
+        if (!resultados.isEmpty()) {
+            logger.info("Resultados encontrados: {}", resultados);
+            return ResponseEntity.ok(resultados);
+        } else {
+            logger.warn("No se encontraron resultados para clave de cotización: {} y nombre de seguro: {}", cotizacionClave, nombreSeguro);
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    
+
+}
